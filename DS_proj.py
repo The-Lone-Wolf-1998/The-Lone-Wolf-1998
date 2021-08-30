353a7c71d7ccda3b39902672bd68b502d0b18814
The-Lone-Wolf-1998 <88648754+The-Lone-Wolf-1998@users.noreply.github.com>
2021-08-30 18:15:47 +0530

    Add files via upload

---
+++ DS_proj.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import glassdoor_scraper as gs
+import pandas as pd
+path = "C:/Users/Poulami/Documents/new_proj_dscience/chromedriver.exe"
+df = gs.get_jobs("Data Science", 10, False, path, 15)
